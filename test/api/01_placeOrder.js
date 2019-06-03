04e68abb2d9d473a3dc19c4047325cd4851035fc
Piyawat Chanthanasombut <piyawat.chanthanasombut@sc.com>
2019-06-03 09:10:59 +0800

    Add simple create order now test

--- test/api/01_placeOrder.js
+++ test/api/01_placeOrder.js
@@ -9,9 +9,7 @@ const expect = chai.expect;
 
 describe('01. API place order', function() {
     describe('POST /orders', function() {
-        it('This is a temporary test', function(done) {
-            // TODO: make this look cleaner
-
+        it('Now order can be created', function(done) {
             chai.request(config.server).post(config.apiPlaceOrder)
                 .send({
                     stops: [constant.place.mongKokStn, constant.place.tstStn]
@@ -20,27 +18,22 @@ describe('01. API place order', function() {
                     expect(err).to.be.null;
                     expect(res).to.have.status(201);
 
-                    // TODO: validate response
-                    expect(res.body).to.include.keys('id');
-                    expect(res.body).to.include.keys('drivingDistancesInMeters');
-                    expect(res.body).to.include.keys('fare');
-                    console.log('PLACE', res.body);
-
+                    expect(res.body).to.have.all.keys('id', 'drivingDistancesInMeters', 'fare');
                     expect(res.body['id']).to.be.a('number');
                     expect(res.body['drivingDistancesInMeters']).to.be.an('array');
                     expect(res.body['fare']).to.be.an('object');
-                    let thisID = res.body.id;
 
-                    // TEMP: cancel it
-                    // TODO: is there a delete-all-order shortcut API ?
-                    chai.request(config.server).put(config.apiCancelOrder(thisID))
-                        .end(function (err, res) {
-                            expect(err).to.be.null;
-                            expect(res).to.have.status(200);
-                            console.log('CANCEL', res.body);
-                            done();
-                        });
+                    done();
                 });
         });
+
+        // TODO: validate fare in day/night time
+        //
+
+        after(function() {
+            // TODO: we have a choice here to clean-up the created order in tests or not
+            // (by cancelling it or other means to delete it ?)
+            //
+        })
     });
 });
